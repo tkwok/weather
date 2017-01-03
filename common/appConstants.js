126b5d669c8710d15bcf4ccc6a245fc01e9124dd
Tony Kwok <holypaladin07@gmail.com>
2017-01-03 13:39:17 -0800

    Update appConstants.js

--- common/appConstants.js
+++ common/appConstants.js
@@ -1,7 +1,7 @@
 /* Created by Tony Kwok on 12/29/2916 */
 (function() {
     weatherApp.constant('appConstants', {
-        WEATHER_API_KEY: "eb26b387e93955f5d1ac1181ece31d23",
+        WEATHER_API_KEY: "",
         WEATHER_API_URL: "http://api.openweathermap.org/data/2.5/forecast/daily",
         DEFAULT_CITY: "San Francisco, CA",
         // define all app constants here 
